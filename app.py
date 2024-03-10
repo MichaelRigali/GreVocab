0f05ae3463db9865532d1ccae53b3743487e033f
MichaelRigali <mikerigali@comcast.net>
2024-03-10 12:38:57 -0400

    microservice reimplemented

--- app.py
+++ app.py
@@ -41,33 +41,33 @@ def generate_vocab_questions(vocabulary, num_questions=5):
         # Randomly select a vocabulary item
         vocab_item = random.choice(vocabulary)
         word = vocab_item['word']
-        # sean's magic
         question = generate_vocab_question(vocabulary, word)
         questions.append(question)
     return questions
 
-# Function to get etymology for a word
 def get_word_details(word):
     try:
         # Making the GET request
-        response = requests.get(f"https://hebererj-cs361-ms-d18cbed0dc46.herokuapp.com/word/{word.lower()}")
+        response = requests.get(f"https://hebererj-cs361-ms-d18cbed0dc46.herokuapp.com/word/{word}")
 
         # Checking if the request was successful (status code 200)
         if response.status_code == 200:
             # Parsing response JSON
             data = response.json()
+            stems = data.get('stems', 'Stems not found.')
             etymology = data.get('etymology', 'Etymology not found.')
-            language_of_origin = data.get('language of origin', 'Language of origin not found.')
+            short_definition = data.get('short_definition', 'Short definition not found.')
             part_of_speech = data.get('part of speech', 'Part of speech not found.')
-            return etymology, language_of_origin, part_of_speech  # Return etymology, language of origin, and part of speech
+            return stems, etymology, short_definition, part_of_speech  # Return stems, etymology, short definition, and part of speech
         else:
             # If there was an error, return error messages for all fields
             return 'Failed to fetch details. Status code: {}'.format(response.status_code), \
                    'Failed to fetch details. Status code: {}'.format(response.status_code), \
+                   'Failed to fetch details. Status code: {}'.format(response.status_code), \
                    'Failed to fetch details. Status code: {}'.format(response.status_code)
     except Exception as e:
         # If there was an exception, return error messages for all fields
-        return 'An error occurred: {}'.format(e), 'An error occurred: {}'.format(e), 'An error occurred: {}'.format(e)
+        return 'An error occurred: {}'.format(e), 'An error occurred: {}'.format(e), 'An error occurred: {}'.format(e), 'An error occurred: {}'.format(e)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -80,10 +80,24 @@ def index():
     incorrect_language_of_origin = ''
     incorrect_part_of_speech = ''
     total_correct = correct_answers_count  # Define total_correct here
+
+    correct_word = None
+    incorrect_stems = None
+    incorrect_etymology = None
+    incorrect_short_definition = None
+    incorrect_part_of_speech = None
+    correct_definition = None
+
+    # Retrieve data from the form submission
+    # user_answer = request.form.get('answer')
+    # correct_answer = request.form.get('correct_answer')
+    # word = request.form.get('word')
+    correct_word = request.form.get('word')  # Retrieve the correct word from the form submission
+    stems, etymology, short_definition, part_of_speech = get_word_details(correct_word)
+    # Assuming you have a function or method to retrieve the correct definition based on the correct word
     
     # If it's a POST request, process the submitted answer
     if request.method == 'POST':
-        print('hello world!')
 
         user_answer = request.form.get('answer')  # Retrieve the selected answer
         correct_answer = request.form.get('correct_answer')  # Retrieve the correct answer
@@ -105,18 +119,20 @@ def index():
         else:
             result = 'Incorrect'
             incorrect_answers_count += 1
-            incorrect_etymology, incorrect_language_of_origin, incorrect_part_of_speech = get_word_details(correct_word)
+            incorrect_etymology, _, incorrect_part_of_speech, _ = get_word_details(correct_word)
             incorrect_answers.append({'word': correct_word, 'definition': correct_answer, 'user_answer': user_answer})  # Store incorrect answer, its definition, and the user's answer
             print(incorrect_answers)
             correct_definition = None  # Define it as None for incorrect answers
 
         # Redirect to the result page after processing the answer
         total_correct = correct_answers_count  # Update total_correct after processing
+        # Redirect or render result page with updated counts and incorrect answers list
         return render_template('result.html', result=result, correct_word=correct_word,
                        correct_answers_count=correct_answers_count, incorrect_answers_count=incorrect_answers_count,
-                       incorrect_answers=incorrect_answers, total_correct=total_correct, total_incorrect=incorrect_answers_count,
-                       incorrect_etymology=incorrect_etymology, incorrect_language_of_origin=incorrect_language_of_origin,
-                       incorrect_part_of_speech=incorrect_part_of_speech, correct_definition=correct_definition)
+                       incorrect_answers=incorrect_answers, total_correct=correct_answers_count, total_incorrect=incorrect_answers_count,
+                       incorrect_stems=incorrect_stems, incorrect_etymology=etymology,
+                       incorrect_short_definition=short_definition, incorrect_part_of_speech=part_of_speech,
+                       correct_definition=correct_definition)
 
     # If it's a GET request, display the quiz question
     categories = ['barron_333']  # Update with your actual category names
@@ -140,22 +156,36 @@ def result():
     global correct_answers_count
     global incorrect_answers_count
     global incorrect_answers
+    correct_word = None
+    incorrect_stems = None
+    incorrect_etymology = None
+    incorrect_short_definition = None
+    incorrect_part_of_speech = None
+    correct_definition = None
 
     # Retrieve data from the form submission
-    user_answer = request.form.get('answer')
-    correct_answer = request.form.get('correct_answer')
-    word = request.form.get('word')
+    # user_answer = request.form.get('answer')
+    # correct_answer = request.form.get('correct_answer')
+    # word = request.form.get('word')
+    correct_word = request.form.get('word')  # Retrieve the correct word from the form submission
+    stems, etymology, short_definition, part_of_speech = get_word_details(correct_word)
+    # Assuming you have a function or method to retrieve the correct definition based on the correct word
+   
+    # Update the incorrect stems variable
+    if isinstance(stems, list):
+        incorrect_stems = ', '.join(stems)  # Join the stems list into a string
+    else:
+        incorrect_stems = stems
+
 
     # Redirect or render result page with updated counts and incorrect answers list
-    return render_template('result.html', result=result, correct_word=word,
-                           correct_answers_count=correct_answers_count,
-                           incorrect_answers_count=incorrect_answers_count,
-                           incorrect_answers=incorrect_answers,
-                           total_correct=correct_answers_count,
-                           total_incorrect=len(incorrect_answers))
+    return render_template('result.html', result=result, correct_word=correct_word,
+                       correct_answers_count=correct_answers_count, incorrect_answers_count=incorrect_answers_count,
+                       incorrect_answers=incorrect_answers, total_correct=correct_answers_count, total_incorrect=incorrect_answers_count,
+                       incorrect_stems=incorrect_stems, incorrect_etymology=etymology,
+                       incorrect_short_definition=short_definition, incorrect_part_of_speech=part_of_speech,
+                       correct_definition=correct_definition)
 
-
-    
 # Generate one vocab question over and over
 def generate_vocab_question(vocabulary: dict, word: str):
     vocabulary_dict = {d['word']: d for d in vocabulary}
