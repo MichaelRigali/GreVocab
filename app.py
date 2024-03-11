1cfc2325fc5068baebe656885abfb664e565c69a
MichaelRigali <mikerigali@comcast.net>
2024-03-11 15:20:25 -0400

    temporary commit

--- app.py
+++ app.py
@@ -131,41 +131,33 @@ def index():
     
     # If it's a POST request, process the submitted answer
     if request.method == 'POST':
-
         user_answer = request.form.get('answer')  # Retrieve the selected answer
         correct_answer = request.form.get('correct_answer')  # Retrieve the correct answer
         correct_word = request.form.get('word')  # Retrieve the correct word
-        # Create if statement for whether or not being quizzed exclusively or not 
+
         if user_answer == correct_answer:
             result = 'Correct'
             correct_answers_count += 1
             correct_definition = correct_answer  # Pass correct definition to template
-            # if user_answer in incorrect_answers:
-            # iterating over dicts in list
-            for word_dict in incorrect_answers:
-                if correct_word in word_dict.values():
-                    # print(f'found in incorrect answers {correct_word}')
-                    # now we want to remove this word from the incorrect answers
-                    incorrect_answers.remove(word_dict)
-                    incorrect_answers_count -= 1
 
+            # Remove correct word from incorrect_answers if present
+            incorrect_answers = [answer for answer in incorrect_answers if answer['word'] != correct_word]
+            
         else:
             result = 'Incorrect'
             incorrect_answers_count += 1
-            # incorrect_etymology, _, incorrect_part_of_speech, _ = get_word_details(correct_word)
-            # stems, etymology, short_definition, part_of_speech
-            incorrect_answers.append({'word': correct_word, 'definition': correct_answer, 'user_answer': user_answer})  # Store incorrect answer, its definition, and the user's answer
+
+            # Add incorrect answer to incorrect_answers list
+            incorrect_answers.append({'word': correct_word, 'definition': correct_answer, 'user_answer': user_answer})
             correct_definition = None  # Define it as None for incorrect answers
 
         # Redirect to the result page after processing the answer
         total_correct = correct_answers_count  # Update total_correct after processing
-        # Redirect or render result page with updated counts and incorrect answers list
-        # Redirect to the result page after processing the answer
         return render_template('result.html', result=result, correct_word=correct_word,
-                               incorrect_answers=incorrect_answers, total_correct=correct_answers_count,
-                               total_incorrect=incorrect_answers_count, incorrect_stems=stems, etymology=etymology,
-                               short_definition=short_definition, part_of_speech=part_of_speech,
-                               correct_definition=correct_definition)
+                            incorrect_answers=incorrect_answers, total_correct=correct_answers_count,
+                            total_incorrect=incorrect_answers_count, incorrect_stems=stems, etymology=etymology,
+                            short_definition=short_definition, part_of_speech=part_of_speech,
+                            correct_definition=correct_definition)
 
     # If it's a GET request, display the quiz question
     categories = ['barron_333']  # Update with your actual category names
